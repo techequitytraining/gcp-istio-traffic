da6fdb069dc9a6628c7776ed81531af1c3f73a5d
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2024-07-21 22:57:06 +0100

    Add option to switch between modes

--- gcp-istio-traffic.sh
+++ gcp-istio-traffic.sh
@@ -80,6 +80,7 @@ cat<<EOF
 ===========================================================================
 Explore Traffic Management, Resiliency and Telemetry Features using Istio 
 ---------------------------------------------------------------------------
+ (0) Switch between Preview, Execute and Delete modes
  (1) Install tools
  (2) Enable APIs
  (3) Create Kubernetes cluster
